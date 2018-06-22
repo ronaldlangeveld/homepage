d6c0f802df1a1f37c0cd52ce5c38d164fc77fdba
Ronald Langeveld <ronaldlangeveld@gmail.com>
2018-06-22 08:40:02 +0200

    added chartjs

--- server.py
+++ server.py
@@ -3,19 +3,23 @@ from flask import render_template
 from jinja2 import Template
 import requests
 import json
+from flask import jsonify, json
+from flask import Response
+from datetime import datetime
+
 app = Flask(__name__)
 
 # @app.route('/')
 # def main():
-#     try:
-#         r = requests.get('https://nomadlist.com/@ronald.json')
-#         raw = r.json()
-#         data = json.dumps(raw['location']['now'])
-#         loaded = json.loads(data)
-#         currentlocation = loaded['country']
-#     except requests.exceptions.ConnectionError as e:
-#         print(e)
-#         currentlocation = "South Africa"
+    # try:
+    #     r = requests.get('https://nomadlist.com/@ronald.json')
+    #     raw = r.json()
+    #     data = json.dumps(raw['location']['now'])
+    #     loaded = json.loads(data)
+    #     currentlocation = loaded['country']
+    # except requests.exceptions.ConnectionError as e:
+    #     print(e)
+    #     currentlocation = "South Africa"
 
         # return render_template('index.html', now=currentlocation)
 
@@ -28,6 +32,55 @@ def main():
     return render_template('index.html', now=currentlocation)
 
 
+@app.route('/lab')
+def lab():
+    
+    return render_template('lab.html')
+
+
+@app.route('/lab/travel')
+def travel():
+    
+    return render_template('travel.html')
+
+@app.route('/api/travel')
+def traveldata():
+    r = requests.get('https://nomadlist.com/@ronald.json')
+    raw = r.json()
+    data = json.dumps(raw['trips'])
+    loaded = json.loads(data)
+    date_format = "%Y-%m-%d"
+
+
+
+    travelData = []
+    for a in loaded:
+        country = a['country']
+        countrycode = a['country_code']
+        start = a['date_start']
+        end = a['date_end']
+        a = datetime.strptime(start, date_format)
+        b = datetime.strptime(end, date_format)
+        delta = b-a
+        tripdays = delta.days
+        # print(delta.days)
+
+
+        newjson =  {
+            "country":country,
+            "countrycode": countrycode,
+            "days": tripdays
+        }
+        
+
+        travelData.append(newjson)
+        jsonStr = json.dumps(travelData)
+        qwe = json.loads(jsonStr)
+
+    return jsonify(Travels=qwe)
+
+
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- static/js/main.js
+++ static/js/main.js
@@ -46,3 +46,82 @@ $( "#twitter-btn" ).hover(function() {
     $('#nightmode').hide();
     $('#lightmode').show();
   })
+
+
+
+  var visitdays;
+var labels
+  if($("#travel").length > 0){
+    $.get( "/api/travel", function( data ) {
+
+       var labels = data.Travels.map(function(a) { 
+          console.log(a.countrycode);
+          return a.country;
+
+        });
+
+        var visitdays = data.Travels.map(function(a) { 
+          console.log(a.days);
+          return a.days;
+
+        });
+
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '# of Days in each country',
+                    data: visitdays,
+                    // backgroundColor: [
+                    //     'rgba(255, 99, 132, 0.2)',
+                    //     'rgba(54, 162, 235, 0.2)',
+                    //     'rgba(255, 206, 86, 0.2)',
+                    //     'rgba(75, 192, 192, 0.2)',
+                    //     'rgba(153, 102, 255, 0.2)',
+                    //     'rgba(255, 159, 64, 0.2)'
+                    // ],
+                    // borderColor: [
+                    //     'rgba(255,99,132,1)',
+                    //     'rgba(54, 162, 235, 1)',
+                    //     'rgba(255, 206, 86, 1)',
+                    //     'rgba(75, 192, 192, 1)',
+                    //     'rgba(153, 102, 255, 1)',
+                    //     'rgba(255, 159, 64, 1)'
+                    // ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+
+    });
+
+  };
+
+  //  var travedata = $.get( "/api/travel", function( data ) {
+
+  //   return data;
+  //  });
+
+  //  console.log(travedata);
+
+  //      var labels = travedata.Travels.map(function(a) { 
+  //         console.log(a.countrycode);
+  //         return a.countrycode;
+  //      });
+
+
+
+
+
+
